56e165afc5ad431f5c16d71f0890f3472e227d49
milad-kalhori <milad7kalhori@gmail.com>
2021-11-08 14:21:42 +0330

    Initial Commit

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -7,19 +7,19 @@ const API_key ="b45da14dc267921b9f079b759ee57d02";
 
 exports.getNowWeathermap = catchAsync (async (req,res,next) => {       
     const {city} = req.body;   
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`).then(function(response) {
+    await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`).then(catchAsync(async function(response) {
       const {weather,wind,clouds} = response.data;
       
       console.log(weather);
       console.log(wind);
       console.log(clouds);
 
-      const data = Weather.create({
+      const data = await Weather.create({
         weather,
         wind,
         clouds
         }); 
-    });
+    }));
     res.status(200).json({
         success : true,
         msg : 'getNowWeathermap'
@@ -27,23 +27,23 @@ exports.getNowWeathermap = catchAsync (async (req,res,next) => {
 });
 
 exports.getTenLatestWeathermap = catchAsync (async (req,res,next) => {           
-    cron.schedule('*/1 * * * *', () => {
+    cron.schedule('*/1 * * * *',catchAsync (async () => {
         console.log('running a task every one minutes');
         const {city} = req.body;
-        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`).then(function(response) {
+        await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`).then (catchAsync(async function(response) {
             const {weather,wind,clouds} = response.data;
 
             console.log(weather);
             console.log(wind);
             console.log(clouds);
 
-            const data = Weather.create({
+            const data = await Weather.create({
                 weather,
                 wind,
                 clouds
             }); 
-        });
-    });
+        }));
+    }));
     res.status(200).json({
         success : true,
         msg : 'getTenLatestWeathermap',
