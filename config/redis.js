f8cfbf077df44e6d545fbe643d7919c74f2c5114
milad-kalhori <milad7kalhori@gmail.com>
2021-11-09 09:17:28 +0330

    Update redis

---
+++ config/redis.js
@@ -0,0 +1,30 @@
+const Redis = require('ioredis');
+const catchAsync = require('../utils/catchAsync');
+
+const redisDemo = catchAsync(async () => {
+    // Connect to Redis at 127.0.0.1, port 6379.
+    const redisClient = new Redis({
+        host: '127.0.0.1',
+        port: 6379,
+    });
+
+
+    const {weather,wind,clouds} = response.data;
+
+    let array = [];
+    if(array.length >= 10) {
+        array.shift();
+    } else {
+        array.push({weather,wind,clouds});
+    }
+    
+    // Set key "myname" to have value "Simon Prickett".
+    await redisClient.hmset('weather',array);
+
+    // Get the value held at key "myname" and log it.
+    const value = await redisClient.hmget('weather');
+    console.log(value);
+
+    // Disconnect from Redis.
+    redisClient.quit();
+});
--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -2,15 +2,13 @@ const Weather = require('../models/weather');
 const axios = require('axios');
 const catchAsync = require('../utils/catchAsync');
 const cron = require('node-cron');
-const Redis = require('ioredis');
-
+const redisDemo = require('../config/redis');
 
 const API_key ="b45da14dc267921b9f079b759ee57d02";
 
 exports.getNowWeathermap = catchAsync (async (req,res,next) => {       
     const {city} = req.body;   
     await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`).then(catchAsync(async function(response) {
-      const {weather,wind,clouds} = response.data;
       
       console.log(weather);
       console.log(wind);
@@ -39,32 +37,8 @@ exports.getTenLatestWeathermap = catchAsync (async (req,res,next) => {
             console.log(wind);
             console.log(clouds);
 
-            const redisDemo = catchAsync(async () => {
-                // Connect to Redis at 127.0.0.1, port 6379.
-                const redisClient = new Redis({
-                    host: '127.0.0.1',
-                    port: 6379,
-                });
-
-                let array = [];
-                if(array.length >= 10) {
-                    array.shift();
-                } else {
-                    array.push({weather,wind,clouds});
-                }
-
-                // Set key "myname" to have value "Simon Prickett".
-                await redisClient.hmset('weather',array);
-
-                // Get the value held at key "myname" and log it.
-                const value = await redisClient.hmget('weather');
-                console.log(value);
-
-                // Disconnect from Redis.
-                redisClient.quit();
-            });
-
-                redisDemo();
+            // connect to redis
+            redisDemo(response.data);
             
         }));
     }));
@@ -83,3 +57,4 @@ exports.getWeathermapByTime = catchAsync (async (req,res,next) => {
         msg : 'getWeathermapByTime',
     }) 
 });
+
