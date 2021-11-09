d72117600dcb1cd361ab6cfa10933d4bc15487c3
milad-kalhori <milad7kalhori@gmail.com>
2021-11-09 16:35:55 +0330

    Updated redisGet.js

--- config/redisGet.js
+++ config/redisGet.js
@@ -2,7 +2,6 @@ const Redis = require('ioredis');
 const catchAsync = require('../utils/catchAsync');
 
 const redisDemo = catchAsync(async (req) => {
-
     // Connect to Redis at 127.0.0.1, port 6379.
     const redisClient = new Redis({
         host: '127.0.0.1',
@@ -14,11 +13,13 @@ const redisDemo = catchAsync(async (req) => {
     // Get the value held at key "myname" and log it.
     const value = await redisClient.get('weather');
     
+    //TODO
     const data = value.filter((item)=>{
         return ((start<item.createdAt) && (end>item.createdAt) ) 
     });
 
     console.log(data);
+
 });
 
 module.exports = redisDemo;
