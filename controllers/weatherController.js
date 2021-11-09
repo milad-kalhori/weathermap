281928bca4bffc673dae5b5ebb54a4e301fa3055
milad-kalhori <milad7kalhori@gmail.com>
2021-11-09 13:23:37 +0330

    Update Controller

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -2,7 +2,8 @@ const Weather = require('../models/weather');
 const axios = require('axios');
 const catchAsync = require('../utils/catchAsync');
 const cron = require('node-cron');
-const redisDemo = require('../config/redis');
+const redisDemoSet = require('../config/redisSet');
+const redisDemoGet = require('../config/redisGet');
 
 const API_key ="b45da14dc267921b9f079b759ee57d02";
 
@@ -27,7 +28,7 @@ exports.getNowWeathermap = catchAsync (async (req,res,next) => {
 });
 
 exports.getTenLatestWeathermap = catchAsync (async (req,res,next) => {           
-    cron.schedule('*/1 * * * *',catchAsync (async () => {
+    cron.schedule('*/20 * * * * *',catchAsync (async () => {
         console.log('running a task every one minutes');
         const {city} = req.body;
         await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`).then (catchAsync(async function(response) {
@@ -38,22 +39,22 @@ exports.getTenLatestWeathermap = catchAsync (async (req,res,next) => {
             console.log(clouds);
 
             // connect to redis
-            redisDemo(response);
-            
+            redisDemoSet(response);      
         }));
     }));
     res.status(200).json({
         success : true,
         msg : 'getTenLatestWeathermap',
     })
-
 });
 
-
 exports.getWeathermapByTime = catchAsync (async (req,res,next) => {
-    
+    // connect to redis
+    redisDemoSet(req.body);
+
     res.status(200).json({
         success : true,
         msg : 'getWeathermapByTime',
     }) 
 });
+
