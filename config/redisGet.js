3c120ed826aff1d22d3faf7d2aa4f04c4f6031cf
milad-kalhori <milad7kalhori@gmail.com>
2021-11-10 09:05:09 +0330

    Updated redisGet.js

--- config/redisGet.js
+++ config/redisGet.js
@@ -7,19 +7,15 @@ const redisDemo = catchAsync(async (req) => {
         host: '127.0.0.1',
         port: 6379,
     });
-
-    const {start,end} = req.body;
     
+    const {start,end} = req.body;
+
     // Get the value held at key "myname" and log it.
     const value = await redisClient.get('weather');
-    
-    //TODO
-    const data = value.filter((item)=>{
-        return ((start<item.createdAt) && (end>item.createdAt) ) 
-    });
 
+    const data =JSON.parse(value).filter((item) => {return (start<=item.createdAt) && (end>=item.createdAt)})
+   
     console.log(data);
-
 });
 
 module.exports = redisDemo;
