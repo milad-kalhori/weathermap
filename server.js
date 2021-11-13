4b5e750a78033a74a5c07f4326ce75a6b2c095d6
milad-kalhori <milad7kalhori@gmail.com>
2021-11-13 10:48:46 +0330

    Added Socket to server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const app = require('./app');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const io = require('socket.io');
 
 dotenv.config({path:'./config/config.env'});
 
 const DB = process.env.DATABASE_LOCAL;
 
 mongoose.connect(DB, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
   .then(() => console.log('DB connection successful!'));
 
@@ -18,10 +19,26 @@ const server = app.listen(PORT,()=>{
     console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT} `);
 });
 
+const socket = io(server);
+const mySocket = socket.of('/socket');
+
+mySocket.on('connection',(socket)=>{
+  console.log("connection")
+
+  socket.on('newMessage',(message)=>{
+    console.log(message.msg)
+    // save message in DB
+  })
+
+  socket.on('disconnection',()=>{
+    console.log('disconnection')
+  })
+})
+
+
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
     console.log(`Error: ${err.message}`);
     // Close server & exit process
     server.close(() => process.exit(1));
   });
-  
